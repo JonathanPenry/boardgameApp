b048697132351e6b4edac068e595abe78c10d3b5
Jonathan Penry <jonathan.penry@gmail.com>
2021-02-26 15:42:04 -0600

    Fixes errors on LoginPage.

--- src/Components/Login/LoginPage.js
+++ src/Components/Login/LoginPage.js
@@ -7,37 +7,38 @@ const LoginPage = (props) => {
     const [password, setPassword] = useState("");
     const history = useHistory();                    //useHistory to push user to the search page
 
-    function login(username, password) {            //Pass username and password into the function
-        console.log(username, password);            //Verify we are seeing what user inputs
+    function login() {                              //Don't pass (username, password) because it just looks for them in local scope instead of global
+        console.log(username, password);            //Verify user inputs are showing up
         if (username.length > 3 && password.length > 3) {
             history.push("/search");                //Push user to search page since no backend...
         }
     }
 
     return (
+        <>
         <section className="formContainer">
             <h3>Please enter your username and password:</h3>
+            <div>
             <label htmlFor="username">User Name: </label>
             <input
                 type="text"
                 id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            >Username: </input>
+                onChange={(e) => setUsername(e.target.value)} 
+                value={username} />
+            </div>
+            <div>
             <label htmlFor="password">Password: </label>
             <input
                 type="text"
                 id="password"
-                value={password}
                 onChange={(e) => setPassword(e.target.value)}
-            >Password: </input>
-
-            <button
-                type="submit"
-                value="submit"
-                onClick={() => login()}             //Calls the login funciton which checks and pushes
-            >Submit</button>
+                value={password} />
+            </div>
+            <div>
+            <button onClick ={() => login()}>Submit</button>
+            </div>
         </section>
+        </>
     )
 
 }
