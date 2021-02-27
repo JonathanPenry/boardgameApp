8f661201dc4e045f114354e986eb6ea0ab4b30c7
Jonathan Penry <jonathan.penry@gmail.com>
2021-02-27 13:44:34 -0600

    RandomPage component updated and displays images.

--- src/Components/Random/RandomPage.js
+++ src/Components/Random/RandomPage.js
@@ -8,35 +8,41 @@ const RandomPage = (props) => {
     
     async function randomGame(search, error) {
         const key = "CdoIKzHYo3"
-        const url = `https://api.boardgameatlas.com/api/search?client_id=${key}&random&limit=1`;
+        const url = `https://api.boardgameatlas.com/api/search?client_id=CdoIKzHYo3&random=true&limit=1`;
+        // const url = `https://api.boardgameatlas.com/api/search?client_id=${key}&random&limit=1`;
         try {
             setError("");
             let response = await fetch(url);            //Uses fetch method on the url provided and assigns to "response"
             let json = await response.json();           //Fetch "response" is converted to json format using json() method
-            let resGames = json.games.map((val) => {    //json data has the values mapped to "resGames" variable
-            return { id: val.id, name: val.name, image: val.image_url, desciption: val.description_preview };
-        });
-        // props.setSearch(resGames);    //commenting out this line gets the console logs to work on next 2 lines
+            let resGames = json.games[0].image_url;
+        
+            //     let resGames = json.games.map((val) => {    //json.games.map((val) => {
+        //     return { id: val.id, name: val.name, image: val.image_url, desciption: val.description_preview };
+        // });
+        setSearch(resGames);
+        console.log(Object.keys(resGames))
         console.log(resGames);                    
         console.log("This line is after console.log(resGames) on RandomPage");
     } catch (e) {
         setError("There was an error");
-        // props.setSearch([]);          //There isn't a search here so it seems unnecessary
+        setSearch([]);
     }
 }
 useEffect(() => randomGame(), []);
 
 
-//Changing {search.id} to "resGames.id" gets undefined.
 return (
         <div className="randomContainer">
         <h1>Random Container</h1>
-        <h3>{search.id}</h3>   
+        <img src={search} />
+
+        {/* <h3>{search.id}</h3>   
         <h3>{search.name}</h3>    
         <img src={search.image} />
-        <p>{search.desciption}</p>
+        <p>{search.desciption}</p> */}
         </div>
     );
 }
+//Changing {search.id} to "resGames.id" gets undefined.
 
 export default RandomPage;
