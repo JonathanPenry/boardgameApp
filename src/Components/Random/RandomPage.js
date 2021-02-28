aecfa3f405fcc94fb7fe44bd86857da6c4c72263
Jonathan Penry <jonathan.penry@gmail.com>
2021-02-28 16:55:04 -0600

    Adds fade-in transition for RandomGame and installs React Spring.

--- src/Components/Random/RandomPage.js
+++ src/Components/Random/RandomPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './RandomPage.css';
+import { Spring } from 'react-spring/renderprops'
 
 const RandomPage = (props) => {
     const [search, setSearch] = useState("");
@@ -28,11 +29,32 @@ const RandomPage = (props) => {
         <div className="randomContainer textCenter">
             <h2 className="marginBtm0">Not sure what to search for?</h2>
             <p className="marginTop0">(Click for another random suggestion...)</p>
-            <img className="imgSize"
-                onClick={() => randomGame()}
-                src={search} />
+            <Spring
+            from={{ opacity: 0 }}
+            to={{ opacity: 1 }}
+            config={{ delay: 1000, duration: 5000 }}>
+                {props => (
+                    <div style={props}>
+                        <img className="imgSize"
+                            onClick={() => randomGame()}
+                            src={search} />
+                    </div>
+                )}
+            </Spring>
         </div>
     );
 }
 
 export default RandomPage;
+
+//ORIGINAL PRE SPRING
+// return (
+//     <div className="randomContainer textCenter">
+//         <h2 className="marginBtm0">Not sure what to search for?</h2>
+//         <p className="marginTop0">(Click for another random suggestion...)</p>
+//         <img className="imgSize"
+//             onClick={() => randomGame()}
+//             src={search} />
+//     </div>
+
+// );
